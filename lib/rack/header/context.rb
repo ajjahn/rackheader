33d42576ec66466c4ecdd4b99c3898085595637c
Koen Punt <koen@koenpunt.nl>
2015-10-30 16:37:26 +0100

    add support to set headers with ENV variables
    environment variables starting with HTTP_ are now passed along as response headers.
    this to support conditional response for services like heroku

--- lib/rack/header/context.rb
+++ lib/rack/header/context.rb
@@ -1,16 +1,25 @@
 module Rack::Header
   class Context
     def initialize(app, headers = {})
-      @app, @headers = app, headers
+      @app = app
+
+      # Map HTTP_* environment variables to response headers
+      @headers = Hash[*ENV.select {|k,v| k.start_with? 'HTTP_'}
+                          .collect {|k,v| [k.sub(/^HTTP_/, ''), v]}
+                          .collect {|k,v| [k.split('_').collect(&:capitalize).join('-'), v]}
+                          .flatten]
+
+      @headers = headers.merge(@headers)
     end
     
     def call(env)
       status, headers, response = @app.call(env)
+
       @headers.each do |k,v|
         headers[k] = v
       end
       headers.delete_if { |k,v| v.nil? }
-      
+
       [status, headers, response]
     end
   end
